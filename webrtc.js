347f94fec597a7a7c364c0e1c78fef0bde77f64f
jelisejev <pavel.yeliseev@ctco.lv>
2016-11-09 11:54:12 +0200

    added "waiting for peer" message

--- webrtc.js
+++ webrtc.js
@@ -1,7 +1,13 @@
 var localVideo;
 var remoteVideo;
 var peerConnection;
+var userId = (Math.random(0, 1) * 1000000).toFixed().toString();
 var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]};
+
+var MESSAGE_TYPE_SDP = 'sdp';
+var MESSAGE_TYPE_ICE = 'ice';
+var MESSAGE_TYPE_ONLINE = 'online';
+var MESSAGE_TYPE_ONLINE_CONFIRM = 'onlineConfirm';
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
@@ -14,6 +20,9 @@ function pageReady() {
 
     serverConnection = new WebSocket('wss://webrtc-proto-server.herokuapp.com/');
     serverConnection.onmessage = gotMessageFromServer;
+    serverConnection.onopen = function() {
+        serverMessage(MESSAGE_TYPE_ONLINE);
+    }
 
     var constraints = {
         video: true,
@@ -47,25 +56,51 @@ function gotMessageFromServer(message) {
     if(!peerConnection) start(false);
 
     var signal = JSON.parse(message.data);
-    if(signal.sdp) {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
-            peerConnection.createAnswer(gotDescription, errorHandler);
-        }, errorHandler);
-    } else if(signal.ice) {
-        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
+
+    // ignore our own messages
+    if (signal.userId === userId) {
+        return;
+    }
+
+    console.log(message);
+
+    switch (signal.type) {
+        case MESSAGE_TYPE_SDP:
+            peerConnection.setRemoteDescription(new RTCSessionDescription(signal.contents.description), function() {
+                peerConnection.createAnswer(gotDescription, errorHandler);
+            }, errorHandler);
+
+            break;
+        case MESSAGE_TYPE_ICE:
+            peerConnection.addIceCandidate(new RTCIceCandidate(signal.contents.candidate));
+            
+            break;
+        case MESSAGE_TYPE_ONLINE:
+            enableCall();
+            serverMessage(MESSAGE_TYPE_ONLINE_CONFIRM);
+            
+            break;
+        case MESSAGE_TYPE_ONLINE_CONFIRM:
+            enableCall();
+            
+            break;
     }
 }
 
 function gotIceCandidate(event) {
     if(event.candidate != null) {
-        serverConnection.send(JSON.stringify({'ice': event.candidate}));
+        serverMessage(MESSAGE_TYPE_ICE, {
+            candidate: event.candidate
+        });
     }
 }
 
 function gotDescription(description) {
     console.log('got description');
     peerConnection.setLocalDescription(description, function () {
-        serverConnection.send(JSON.stringify({'sdp': description}));
+        serverMessage(MESSAGE_TYPE_SDP, {
+            description: description
+        });
     }, function() {console.log('set description error')});
 }
 
@@ -76,4 +111,17 @@ function gotRemoteStream(event) {
 
 function errorHandler(error) {
     console.log(error);
+}
+
+function serverMessage(type, contents) {
+    serverConnection.send(JSON.stringify({
+        type: type,
+        userId: userId,
+        contents: contents
+    }));
+}
+
+function enableCall() {
+    document.querySelector('#waiting').style.display = 'none';
+    document.querySelector('#start').style.display = 'inline';
 }
